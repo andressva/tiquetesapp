d9d39bd1b4d4807e5741de8ab27aec84b73dff18
Andres Vergel <amvergel@hotmail.com>
2021-02-13 12:49:26 -0500

    placeholder and options input

--- components/Searcher/Searcher.js
+++ components/Searcher/Searcher.js
@@ -12,15 +12,21 @@ export default function Searcher() {
   const onSubmit = data => {
     searchFlights(data.criteria.trim());
   }
-
   return (
+    <div className={styles.wrap}>
     <form className={styles.searcher} id="searchForm" onSubmit={handleSubmit(onSubmit)}>
       <label className={styles.label} htmlFor="criteria" >busca por codigo de reserva:</label>
       <div className={styles.inputWrap}>
-          <input className={styles.input} maxLength="10" name="criteria" ref={register({ required: true })} />
+          <input className={styles.input} list="codes" placeholder="Ej: RK2V89" maxLength="10" name="criteria" ref={register({ required: true })} />
+          <datalist id="codes">
+            <option value="RK2V89"/>
+            <option value="MO4737"/>
+          </datalist>
           <button className={styles.btn} type="submit" form="searchForm" ><FaSearch /></button>
       </div>
       {errors.criteria && <span className={styles.error}>Este campo es requerido</span>}
     </form>
+    <span></span>
+    </div>
   )
 }
